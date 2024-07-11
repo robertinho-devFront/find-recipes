211ae9e25203ccddfe37078cd20a1d53ce939c02
robertinho-devFront <rdasilva75@gmail.com>
2024-07-11 17:45:31 +0200

    reset

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,23 +1,23 @@
 export const RecipeCard = (recipe) => {
-    return `
-      <div class="recipe-card">
-        <img src="recipe/${recipe.image}" alt="${recipe.name}" class="recipe-card__image">
-        <div class="recipe-card__details">
-          <h2 class="recipe-card__title">${recipe.name}</h2>
-          <p class="recipe-card__time"><strong></strong> ${recipe.time} min</p>
-          <p class="recipe-card__description"><strong>Recette</strong> ${recipe.description}</p>
-          <p class="recipe-card__ingredients"><strong>Ingrédients</strong> </p>
-          <ul class="recipe-card__list">
-            ${recipe.ingredients.map(ingredient => `
-              <li class="recipe-card__list-item">
-                <span>${ingredient.ingredient}</span>
-                <span class="ingredient">${ingredient.quantity ? ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""}</span>
-              </li>
-            `).join("")}
-          </ul>
-        </div>
+  return `
+    <div class="recipe-card">
+      <img src="recipe/${recipe.image}" alt="${recipe.name}" class="recipe-card__image">
+      <div class="recipe-card__details">
+        <h2 class="recipe-card__title">${recipe.name}</h2>
+        <p class="recipe-card__time"><strong></strong> ${recipe.time} min</p>
+        <p class="recipe-card__description"><strong>Recette</strong> ${recipe.description}</p>
+        <p class="recipe-card__ingredients"><strong>Ingrédients</strong> </p>
+        <ul class="recipe-card__list">
+          ${recipe.ingredients.map(ingredient => `
+            <li class="recipe-card__list-item">
+              <span>${ingredient.ingredient}</span>
+              <span class="ingredient">${ingredient.quantity ? ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""}</span>
+            </li>
+          `).join("")}
+        </ul>
       </div>
-    `;
-  };
+    </div>
+  `;
+};
 
-  export default RecipeCard
+export default RecipeCard
--- script.new-way.js
+++ script.new-way.js
@@ -26,4 +26,3 @@ export const displayPage = (recipes) => {
 
   displayPage(recipesFiltered);
 })();
-
