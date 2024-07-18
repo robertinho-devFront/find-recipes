c2ee81da22bbf7e4f20ab8bb5a607eb2ddd844a4
robertinho-devFront <rdasilva75@gmail.com>
2024-07-18 20:30:05 +0200

    ingr apareil, tools

--- script.new-way.js
+++ script.new-way.js
@@ -1,3 +1,51 @@
+// import { getRecipes } from "./api.js";
+
+// import Header from "./components/header.js";
+// import Filters from "./components/Filters.js";
+// import RecipeCard from "./components/RecipeCard.js";
+
+// import filterRecipes from "./utils/filterRecipes.js";
+
+
+// import getFiltersFromURLSearchParams, {URL_PARAMS} from './utils/getFiltersFromURLSearchParams.js'
+
+// const ActiveFilters = () => {
+//   const currentFilters = getFiltersFromURLSearchParams();
+
+//   const ingredients = currentFilters[URL_PARAMS.INGREDIENTS];
+//   const ustensil = currentFilters[URL_PARAMS.USTENSIL];
+//   const tools = currentFilters[URL_PARAMS.TOOLS];
+//   const search = currentFilters[URL_PARAMS.SEARCH];
+  
+//   return `${ingredients.map(ingredient => `<span>${ingredient}</span>`).join("")}`
+//   return `${tools.map(tools => `<span>${tools}</span>`).join("")}`
+//   return `${ustensil.map(ustensil => `<span>${ustensil}</span>`).join("")}`
+//   return `${search.map(search => `<span>${search}</span>`).join("")}`
+// };
+
+// export const displayPage = (recipes) => {
+//   const app = document.querySelector("#app");
+
+//   app.innerHTML = `
+//       ${Header()}
+//       ${Filters(recipes)}
+//       ${ActiveFilters()}
+//       <section id="recipes-container">
+//         ${recipes.map((recipe) => RecipeCard(recipe)).join("")}
+//       </section>
+//   `;
+// };
+
+// (async () => {
+//   const initialRecipes = await getRecipes();
+
+//   const recipesFiltered = filterRecipes(initialRecipes);
+
+//   displayPage(recipesFiltered);
+// })();
+
+
+
 import { getRecipes } from "./api.js";
 
 import Header from "./components/header.js";
@@ -6,15 +54,22 @@ import RecipeCard from "./components/RecipeCard.js";
 
 import filterRecipes from "./utils/filterRecipes.js";
 
-
-import getFiltersFromURLSearchParams, {URL_PARAMS} from './utils/getFiltersFromURLSearchParams.js'
+import getFiltersFromURLSearchParams, { URL_PARAMS } from './utils/getFiltersFromURLSearchParams.js';
 
 const ActiveFilters = () => {
   const currentFilters = getFiltersFromURLSearchParams();
 
   const ingredients = currentFilters[URL_PARAMS.INGREDIENTS];
-  
-  return `${ingredients.map(ingredient => `<span>${ingredient}</span>`).join("")}`
+  const ustensil = currentFilters[URL_PARAMS.USTENSIL];
+  const tools = currentFilters[URL_PARAMS.TOOLS];
+  const search = currentFilters[URL_PARAMS.SEARCH];
+
+  return `
+    ${ingredients.map(ingredient => `<span>${ingredient}</span>`).join("")}
+    ${tools.map(tool => `<span>${tool}</span>`).join("")}
+    ${ustensil.map(ust => `<span>${ust}</span>`).join("")}
+    ${search ? `<span>${search}</span>` : ''}
+  `;
 };
 
 export const displayPage = (recipes) => {
@@ -23,7 +78,9 @@ export const displayPage = (recipes) => {
   app.innerHTML = `
       ${Header()}
       ${Filters(recipes)}
-      ${ActiveFilters()}
+      <div id="active-filters">
+        ${ActiveFilters()}
+      </div>
       <section id="recipes-container">
         ${recipes.map((recipe) => RecipeCard(recipe)).join("")}
       </section>
@@ -32,7 +89,6 @@ export const displayPage = (recipes) => {
 
 (async () => {
   const initialRecipes = await getRecipes();
-
   const recipesFiltered = filterRecipes(initialRecipes);
 
   displayPage(recipesFiltered);
--- utils/getFiltersFromURLSearchParams.js
+++ utils/getFiltersFromURLSearchParams.js
@@ -11,8 +11,12 @@ export const getFiltersFromURLSearchParams = () => {
 
   return {
     [URL_PARAMS.SEARCH]: params.get(URL_PARAMS.SEARCH) || "",
-    [URL_PARAMS.TOOLS]: params.get(URL_PARAMS.TOOLS) ? params.get(URL_PARAMS.TOOLS).split(",") : [],
-    [URL_PARAMS.USTENSIL]: params.get(URL_PARAMS.USTENSIL) ? params.get(URL_PARAMS.USTENSIL).split(",") : [],
+    [URL_PARAMS.TOOLS]: params.get(URL_PARAMS.TOOLS) 
+      ? params.get(URL_PARAMS.TOOLS).split(",") 
+      : [],
+    [URL_PARAMS.USTENSIL]: params.get(URL_PARAMS.USTENSIL) 
+      ? params.get(URL_PARAMS.USTENSIL).split(",")
+      : [],
     [URL_PARAMS.INGREDIENTS]: params.get(URL_PARAMS.INGREDIENTS)
       ? params.get(URL_PARAMS.INGREDIENTS).split(",")
       : [],
